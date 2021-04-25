76206b657f8e0b2e7b429b1f865902e4c0197aa4
G4st3r21 <aporyadin003@gmail.com>
2021-04-25 11:03:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ dp.middleware.setup(LoggingMiddleware())
 loop = asyncio.get_event_loop()
 
 path = os.path.abspath('data/Answers/tardis-isbv-a8b739ce96e6.json')
-os.system(f"export GOOGLE_APPLICATION_CREDENTIALS={path}")
-print(f"export GOOGLE_APPLICATION_CREDENTIALS={path}")
+os.system(f'export GOOGLE_APPLICATION_CREDENTIALS="{path}"')
+print(f'export GOOGLE_APPLICATION_CREDENTIALS="{path}"')
 # --------------------other params----------------------- #
 
 state = ''
